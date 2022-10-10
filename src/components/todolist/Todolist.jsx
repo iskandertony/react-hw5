68433bc9e963f874ea932e61225bd1bc7aadd2d4
Iska Mabo <maboiskanderr@gmail.com>
2022-10-10 19:05:25 +0600

    just

--- src/components/todolist/Todolist.jsx
+++ src/components/todolist/Todolist.jsx
@@ -1,34 +1,44 @@
 import "./Todolist.css";
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 
 const Todolist = () => {
   const [valueInput, setValueInput] = useState("");
   const [todolist, setTodolist] = useState([]);
-
+  const [edit, setEdit] = useState(false);
 
   const handleInput = (e) => {
     const value = e.target.value;
     setValueInput(value);
   };
   const handleButton = () => {
-    const newList = [...todolist, {name: valueInput, isDone: false}];
+    const newList = [...todolist, { name: valueInput, isDone: false }];
     setTodolist(newList);
     setValueInput("");
   };
 
   const handleDoneArray = (index) => {
-    const array = todolist.map((item, i) => (
-      { name: item.name, isDone: index === i ? !item.isDone  : item.isDone}
-    ))
-    setTodolist(array)
+    const array = todolist.map((item, i) => ({
+      name: item.name,
+      isDone: index === i ? !item.isDone : item.isDone,
+    }));
+    setTodolist(array);
   };
 
   const handleDelete = (i) => {
-    const array = [...todolist]
-    array.splice(i,1)
-    setTodolist(array)
-  }
+    const array = [...todolist];
+    array.splice(i, 1);
+    setTodolist(array);
+  };
+
+  const handleEdit = (index) => {
+    const array = todolist.map((item, i) => (
+      <div>
+        <input></input>
+      </div>
+    ));
+    setTodolist(array);
+  };
 
   console.log(todolist);
 
@@ -55,13 +65,15 @@ const Todolist = () => {
         {todolist.map((item, index) => (
           <div key={index} className="title">
             <div className="title-sub">
-
               <div>{item.isDone === true ? <s>{item.name}</s> : item.name}</div>
               <div>
+                <button onClick={() => handleEdit(index)}>edit</button>
                 <button className="red" onClick={() => handleDoneArray(index)}>
                   {item.isDone === true ? "Undone" : "Done"}
                 </button>
-                <button className="blue" onClick={() => handleDelete(index)}>Delete</button>
+                <button className="blue" onClick={() => handleDelete(index)}>
+                  Delete
+                </button>
               </div>
             </div>
           </div>
